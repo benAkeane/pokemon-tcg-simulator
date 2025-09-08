1872cfdbb9abf470efe30113ef0927dbd62445e7
benAkeane <benthelen88@gmail.com>
2025-09-08 19:07:33 -0400

    generate_cards() now produces a list of 5 random cards data

--- pokemon_generator.py
+++ pokemon_generator.py
@@ -15,12 +15,12 @@ load_dotenv() # take environment variables from .env
 TOTAL_CARDS = 230
 CARDS_IN_PACK = 5
 SET_LIST = ["swsh12pt5", "swsh12pt5gg"]
-CACHE_DIR = "card_images" #folder where card images are stored
+CACHE_DIR = "card_images" # folder where card images are stored
 POKEMON_TCG_API_KEY = os.getenv('POKEMON_TCG_API')
 PACK_IMG_PATH = "card_images/swsh12pt5pack.png"
-SET_RARITY = {   # Whether a card will be in the base set or galarian gallery extension
-    "swsh12pt5": 0.067,
-    "swsh12pt5gg": 0.933
+SET_RARITY = {    # Set Distribution
+    "swsh12pt5": 0.933,  # Base set
+    "swsh12pt5gg": 0.067 # Galarian Gallery
 }
 BASE_RARITY_TABLE = {  # Rarity values for the base set cards
     "Common": 0.700,
@@ -189,9 +189,43 @@ class CardPack:
     # will do this by calculating the rarities of each card in the pack
     # sort the list by rarity so that the rarest cards are at the end of the pack
     def generate_cards(self):
+        """Generate a list of 5 cards, choosing set and rarity from tables, the pulling from cached pools"""
+        with open("cards_cache.json", "r") as f:
+            cache_data = json.load(f)
 
+        generated_cards = []
+        sets = list(SET_RARITY.keys())
+        set_weights = list(SET_RARITY.values())
 
-        return None
+        for _ in range(CARDS_IN_PACK):
+            # Pick which set
+            chosen_set = random.choices(sets, weights=set_weights, k=1)[0]
+
+            # Pick rarity depending on set
+            if chosen_set == "swsh12pt5":
+                rarities = list(BASE_RARITY_TABLE.keys())
+                weights = list(BASE_RARITY_TABLE.values())
+            else:
+                rarities = list(GG_RARITY_TABLE.keys())
+                weights = list(GG_RARITY_TABLE.values())
+            
+            chosen_rarity = random.choices(rarities, weights=weights, k=1)[0]
+
+            # Pick card id from cards_cache rarity pools
+            rarity_pools = cache_data["sets"][chosen_set]["rarity_pools"]
+            card_pool = rarity_pools.get(chosen_rarity, [])
+            if not card_pool:
+                continue # Skip if no cards exist in this rarity
+
+            chosen_card_id = random.choice(card_pool)
+
+            # Get full card info
+            card_info = cache_data["all_cards"][chosen_card_id]
+            generated_cards.append(card_info)
+        # Store in instance state for later display
+        self.current_cards = generated_cards
+        print(generated_cards)
+        return generated_cards
 
 
     # TODO: Hide Open Pack button, make it so that when the card image is clicked it goes to the next card.
@@ -217,14 +251,10 @@ if not card_data_preloaded:
         save_cache(set_id, all_cards, rarity_pools)
         card_data_preloaded = True
 
-create_rarity_table()
-# all_cards, rarity_pools = load_cache()
-# if not all_cards:
-#     for set in SET_LIST:
-#         all_cards, rarity_pools = preload_card_data(set)
-#         save_cache(all_cards, rarity_pools)
 if not images_preloaded:
     preload_images() # preload at startup
     images_preloaded = True
+
 app = CardPack(root)
+app.generate_cards()
 root.mainloop()
